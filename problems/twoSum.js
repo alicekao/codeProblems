f524fc9c3e5441ce9db80943ada2d4c6b939f903
alicekao <alicekao.ak@gmail.com>
2016-09-13 16:10:38 -0700

    Improved twosum to linear time complexity

--- problems/twoSum.js
+++ problems/twoSum.js
@@ -1,4 +1,19 @@
+// O(n) complexity
 module.exports = (arr, target) => {
+  const storage = {};
+  arr.forEach((el, i) => {
+    storage[el] = i;
+  });
+  for (let i = 0; i < arr.length; i++) {
+    if (storage[target-arr[i]]) {
+      return [i, storage[target-arr[i]]];
+    }
+  }
+  return null;
+}
+
+// O(n^2) complexity
+const bruteForce = (arr, target) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i+1; j < arr.length; j++) {
       if (arr[i] + arr[j] === target) { return [i, j]; }
